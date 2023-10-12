7d91a0bf2cc282d45c4f9d5d55368077b9133d88
mtavasszy <matt.tavasszy@gmail.com>
2023-10-12 23:30:36 +0200

    setup GL, GLFW and GLEW

--- TriMe/main.cpp
+++ TriMe/main.cpp
@@ -1,8 +1,143 @@
-// Your First C++ Program
-
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-int main() {
-    std::cout << "Hello World!";
+const char* vertexShaderSource = R"(
+    #version 450 core
+    in vec3 position;
+    in vec3 color;
+    out vec3 fragColor;
+    uniform mat4 model;
+    void main()
+    {
+        gl_Position = model * vec4(position, 1.0);
+        fragColor = color;
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 450 core
+    in vec3 fragColor;
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(fragColor, 1.0);
+    }
+)";
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    // Create a GLFW window and OpenGL context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Random Triangles", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, keyCallback);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW." << std::endl;
+        return -1;
+    }
+
+    // Define vertices and colors for the triangles
+    float vertices[] = {
+        -0.6f, -0.6f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.6f, -0.6f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.0f,  0.6f, 0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    // Create a Vertex Array Object (VAO) and Vertex Buffer Object (VBO)
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Set up vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Create and compile the vertex and fragment shaders
+    GLuint vertexShader, fragmentShader, shaderProgram;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    // Check for shader compilation errors (you should add error handling here)
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    // Check for shader compilation errors (you should add error handling here)
+
+    // Create shader program and link shaders
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // Check for program linking errors (you should add error handling here)
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Use the shader program
+    glUseProgram(shaderProgram);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear the screen
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Randomly position the triangles
+        for (int i = 0; i < 10; i++)
+        {
+            glm::mat4 model(1.0f);
+            model = glm::translate(model, glm::vec3(rand() % 400 / 100.0f - 2.0f, rand() % 300 / 100.0f - 1.0f, 0.0f));
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteProgram(shaderProgram);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+
     return 0;
 }
